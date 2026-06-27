ee24fef7135ac21a3fa9766a82d7e12e1389b8ae
Fincali197 <alexgo1910@gmail.com>
2026-06-27 15:18:26 +0300

    removed D

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -6,9 +6,6 @@ vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = 'Telescope help tags' })
 
--- Make D be d but no yank
-vim.keymap.set('v', 'D', '"_d', { noremap = true, silent = true })
-
 -- Format current file with leader+f
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
